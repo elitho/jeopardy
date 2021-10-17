e295e3f9817ba7edb95053014e6737f4a224290d
elitho <elias3004@msn.com>
2021-10-17 15:23:55 +0200

    init styled-components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-import './App.css';
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  background: red;
+`
 
 export const App = () => {
-  return <>App</>;
+  return <AppContainer>App</AppContainer>;
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import { App } from './App';
+import {createGlobalStyle} from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    box-sizing: border-box;
+  }
+  a {
+    outline: none;
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+  
+  * {
+    box-sizing: border-box;
+  }
+`
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
